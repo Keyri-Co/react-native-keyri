24a11266b131cfe98d19cca67cdc6907d26c956f
Ruslan Gremechevsky <grem.rusl@gmail.com>
2022-07-14 17:10:57 +0300

    Add linter configs

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 100,
+  endOfLine: 'auto',
+};
