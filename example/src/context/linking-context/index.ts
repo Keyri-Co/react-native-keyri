bf0b25f1afff9dd202244811c9a776b47a0d6466
Ruslan Gremechevsky <grem.rusl@gmail.com>
2022-08-08 17:00:22 +0300

    Implement keyri processLink

--- example/src/context/linking-context/index.ts
+++ example/src/context/linking-context/index.ts
@@ -1,27 +1,48 @@
-import { createContext, useMemo, useState } from 'react';
-import { Linking } from 'react-native';
+import { APP_KEY } from '../../utils/constants';
+import { createContext, useEffect, useMemo, useState } from 'react';
+import { Alert, Linking } from 'react-native';
+import Keyri from 'react-native-keyri';
 
 import type { IAppLinkContext } from '../../utils/types';
 
 export function LinkContext() {
   const [deepLink, setDeepLink] = useState<string>('');
 
-  const linkObj = useMemo(
+  useEffect(() => {
+    Linking.getInitialURL().then((url) => {
+      if (!url) return;
+
+      setDeepLink(url);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!deepLink) return;
+
+    const onPressPrecess = () => {
+      const publicUserId = 'user@email';
+      Keyri.processLink({ appKey: APP_KEY, url: deepLink, publicUserId })
+        .then(() => Alert.alert('Success', 'The link has been processed'))
+        .catch((e) => Alert.alert('Error', e?.message));
+    };
+
+    Alert.alert('Linking', 'Do you want to process a link?', [
+      { text: 'Yes', onPress: onPressPrecess },
+      { text: 'No', style: 'cancel' },
+    ]);
+
+    setDeepLink('');
+  }, [deepLink]);
+
+  return useMemo(
     () => ({
       deepLink,
       setDeepLink,
     }),
     [deepLink]
   );
-  const getInitialUrl = async () => {
-    const initialUrl = await Linking.getInitialURL();
-    if (initialUrl) {
-      setDeepLink(initialUrl);
-    }
-  };
-  getInitialUrl();
-  return linkObj;
 }
+
 export const AppLinkContext = createContext<IAppLinkContext>({
   deepLink: '',
   setDeepLink: (_link: string) => {
