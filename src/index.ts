9a3137bd814bc73cded1151f7950d6c66cbb701e
andriikuliahin <kulagin.andrew38@gmail.com>
2023-09-07 14:44:59 +0300

    Updated libraries, lint, podfile

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import type {
 } from './types';
 
 const LINKING_ERROR =
-  `The package 'react-native-keyri' doesn't seem to be linked. Make sure: \n\n` +
+  "The package 'react-native-keyri' doesn't seem to be linked. Make sure: \n\n" +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
